d40dfab149c4d2c59fab5bb99580f0f66dbf6ad2
Matthew <matteidsen@gmail.com>
2024-02-22 10:05:44 -0800

    initialize navigation buttons

--- resources/script.js
+++ resources/script.js
@@ -16,3 +16,16 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
+function goStore(){
+  console.log("Going to store.");
+};
+
+function goCave(){
+  console.log("Going to cave.");
+}
+
+function fightDragon(){
+  console.log("Fighting dragon.");
+}
